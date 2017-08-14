a40dad2edaeaf3637813e7dc2d27e4d0b02849ba
Eashwar Mohan <luigi629@gmail.com>
2017-08-14 18:46:24 -0400

    let's see what happens

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@ var converter = new showdown.Converter();
 converter.setOption('openLinksInNewWindow', true);
 converter.setFlavor('github');
 
+var post_url = "https://raw.githubusercontent.com/eashwar/eashwar.github.io/master/posts/";
+
 var info_box_text =
 `
 ## hello!
@@ -15,41 +17,25 @@ On this website you can find links to my profiles on various platforms, as well 
 `;
 var info_box_html = converter.makeHtml(info_box_text);
 
-var post_one =
-  `
-## [first post: learning](./posts/post1.html)
 
-I'm writing this post using [\`showdown\`](https://github.com/showdownjs/showdown)!
-It's a javascript library that parses markdown files and inserts them as html.
->"Showdown: it works!"
--the unofficial tag of showdown
+var post_one_html;
+$.get(post_url + "post1.txt", 'text', function (post) {
+    post_two_html = converter.makeHtml(post);
+    loadPostOne();
+    loadPostOneInMain();
+});
 
-\`\`\`
-function test() {
- console.log("success");
-}
-\`\`\`
+var post_two_html;
+$.get(post_url + "post2.txt", 'text', function (post_two) {
+    post_two_html = converter.makeHtml(post_two);
+    loadPostTwo();
+    loadPostThreeInMain();
+});
 
-I'll probably post here more often later, and probably set up an actual blogsite. Until then, this is empty!
-`;
-var post_one_html = converter.makeHtml(post_one);
-
-var post_two =
-`
-## [second post: another one!](./posts/post2.html)
-
-Hey guys! I got the blog set up, so now there can be multiple posts! You can navigate to the blog by clicking on any of the instances of the word 'blog' that aren't in this post. I think that's it for me, hope to post here soon!
-(I know there probably aren't many viewers on this site, so if you're here, that's cool.)
-
-
-`;
-var post_two_html = converter.makeHtml(post_two);
-
-var post_three;
-
-$.get("https://eashwar.github.io/posts/post3.txt", function (my_var) {
-    post_three = my_var;
-}, 'text');
-
-console.log(post_three);
-var post_three_html = converter.makeHtml(post_three);
+var post_three_html;
+$.get(post_url+"post3.txt", 'text', function (post_three) {
+    post_three_html = converter.makeHtml(post_three);
+    loadPostThree();
+    loadPostThreeInMain();
+    loadLatestPost(post_three_html);
+});
