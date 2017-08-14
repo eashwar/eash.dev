838f23fb4f3466f906c9c0c8e12bc1632e3e8f35
Eashwar Mohan <luigi629@gmail.com>
2017-08-14 19:12:02 -0400

    using the right file extension because it works, really happy with this

---
+++ about.md
@@ -0,0 +1,8 @@
+## hello!
+I'm a freshman at the University of Michigan. My interests include:
+* computer science
+* computer music
+* clarinet and piano performance
+* speedrunning
+
+On this website you can find links to my profiles on various platforms, as well as my [resume](https://resume.creddle.io/resume/1r07ere59ch).
--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,3 @@ converter.setOption('openLinksInNewWindow', true);
 converter.setFlavor('github');
 
 var post_url = "https://raw.githubusercontent.com/eashwar/eashwar.github.io/master/posts/";
-
-var info_box_text =
-`
-## hello!
-I'm a freshman at the University of Michigan. My interests include:
-* computer science
-* computer music
-* clarinet and piano performance
-* speedrunning
-
-On this website you can find links to my profiles on various platforms, as well as my [resume](https://resume.creddle.io/resume/1r07ere59ch).
-`;
-var info_box_html = converter.makeHtml(info_box_text);
