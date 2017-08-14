91fd1448f226308248f54bbf74ddf38a21f91793
Eashwar Mohan <luigi629@gmail.com>
2017-08-14 19:02:56 -0400

    this should work

--- scripts/main.js
+++ scripts/main.js
@@ -16,26 +16,3 @@ I'm a freshman at the University of Michigan. My interests include:
 On this website you can find links to my profiles on various platforms, as well as my [resume](https://resume.creddle.io/resume/1r07ere59ch).
 `;
 var info_box_html = converter.makeHtml(info_box_text);
-
-
-var post_one_html;
-$.get(post_url + "post1.txt", 'text', function (post) {
-    post_two_html = converter.makeHtml(post);
-    loadPostOne();
-    loadPostOneInMain();
-});
-
-var post_two_html;
-$.get(post_url + "post2.txt", 'text', function (post_two) {
-    post_two_html = converter.makeHtml(post_two);
-    loadPostTwo();
-    loadPostThreeInMain();
-});
-
-var post_three_html;
-$.get(post_url+"post3.txt", 'text', function (post_three) {
-    post_three_html = converter.makeHtml(post_three);
-    loadPostThree();
-    loadPostThreeInMain();
-    loadLatestPost(post_three_html);
-});
