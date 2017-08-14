bd5aa2d54ce7e9f7d940a4ea16ea61ad14160d32
Eashwar Mohan <luigi629@gmail.com>
2017-08-14 19:29:02 -0400

    separate post CSS, update post datasource
    i'm really happy with how this turned out!

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,4 @@ var converter = new showdown.Converter();
 converter.setOption('openLinksInNewWindow', true);
 converter.setFlavor('github');
 
-var post_url = "https://raw.githubusercontent.com/eashwar/eashwar.github.io/master/posts/";
+var post_url = "https://eashwar.github.io/posts/";
