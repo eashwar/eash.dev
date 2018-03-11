4bed6e1bb657ce132746dafb9d4aa752ea1ebbb9
Eashwar Mohan <eashwar@umich.edu>
2018-03-11 13:09:00 -0400

    implement cookie storage of theme choice

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,23 @@
 ---
 ---
+
 document.getElementById("theme-switch").onclick = function() {
-    console.log(document.getElementById("theme"))
     if (document.getElementById("theme").href == "{{site.url}}/styles/dark.css")
     {
         document.getElementById("theme").href = "{{site.url}}/styles/light.css"
+        Cookies.set('theme', 'light');
     }
     else
     {
         document.getElementById("theme").href = "{{site.url}}/styles/dark.css"
+        Cookies.set('theme', 'dark');
+    }
+};
 
+window.onload = function() {
+    var prevTheme = Cookies.get('theme');
+    if (typeof prevTheme != "undefined")
+    {
+        document.getElementById("theme").href = "{{site.url}}/styles/" + prevTheme + ".css"
     }
 };
