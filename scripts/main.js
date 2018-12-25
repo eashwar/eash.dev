c3324782b7a6d25727e0e4c7945f1cbfb72add61
Eashwar Mohan <eashwar@umich.edu>
2018-12-25 14:18:53 -0500

    some hotfixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
 ---
 ---
 document.getElementById("theme-switch").onclick = function() {
-    if (document.getElementById("theme").href == "{{site.url}}/styles/dark.css")
+    if (document.getElementById("theme").href == "styles/dark.css")
     {
-        document.getElementById("theme").href = "{{site.url}}/styles/light.css"
+        document.getElementById("theme").href = "styles/light.css"
         Cookies.set('theme', 'light');
     }
     else
     {
-        document.getElementById("theme").href = "{{site.url}}/styles/dark.css"
+        document.getElementById("theme").href = "styles/dark.css"
         Cookies.set('theme', 'dark');
     }
 };
