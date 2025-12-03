dc88611b326f8c9ad1d46d92058f701050babc25
eash <luigi629@gmail.com>
2025-12-03 10:44:59 -0800

    Fix formatting issues in extLinks.js

--- scripts/extLinks.js
+++ scripts/extLinks.js
@@ -4,7 +4,7 @@ const extLinks = [
     {href: "https://bsky.app/profile/eash.dev", icon: "fa-brands fa-bluesky"},
     {href: "https://linkedin.com/in/eashw", icon: "fa-brands fa-linkedin"}
 ]
-
+// test
 const putExtLinks = () => {
     let extLinksHtml = extLinks.map(item => {
         return `<a href="${item.href}" target="_blank" rel="noopener noreferrer"><i class="${item.icon}"></i></a>`
