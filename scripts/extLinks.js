ce58c50c88f5a1c71a8e80723531eb176d4c1ff4
eash <luigi629@gmail.com>
2025-12-03 10:34:43 -0800

    Remove commented Bandcamp link from extLinks
    Removed commented-out Bandcamp link from extLinks array.

--- scripts/extLinks.js
+++ scripts/extLinks.js
@@ -2,7 +2,6 @@ const extLinks = [
     {href: "mailto:eash@eash.dev", icon: "fa-solid fa-envelope"},
     {href: "https://linkedin.com/in/eashw", icon: "fa-brands fa-linkedin"},
     {href: "https://github.com/eashwar", icon: "fa-brands fa-github"},
-    // {href: "https://eashwar.bandcamp.com", icon: "fa-brands fa-bandcamp"},
     {href: "https://bsky.app/profile/eash.dev", icon: "fa-brands fa-bluesky"}
 ]
 
